532ecc5979b6f46ce1902aab48da971b4d2225c0
ntakaa-dyt <alfaqihnataka@gmail.com>
2026-06-04 11:46:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,36 @@ top:0,
 behavior:"smooth"
 });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    // Efek saat halaman dibuka
+    document.body.style.opacity = "0";
+
+    setTimeout(() => {
+        document.body.style.opacity = "1";
+    }, 100);
+
+    // Efek saat pindah halaman
+    document.querySelectorAll("a").forEach(link => {
+
+        // Hanya link internal
+        if (
+            link.href &&
+            link.hostname === window.location.hostname
+        ) {
+            link.addEventListener("click", function (e) {
+                e.preventDefault();
+
+                const tujuan = this.href;
+
+                document.body.classList.add("fade-out");
+
+                setTimeout(() => {
+                    window.location.href = tujuan;
+                }, 500);
+            });
+        }
+    });
+
+});
